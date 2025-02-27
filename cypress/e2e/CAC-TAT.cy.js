d801ace6d23992b06e3144841f403bdc63431c9f
davidtmasin <davidteixeira.info@gmail.com>
2025-02-27 15:44:29 -0300

    Trabalhando com get(), type(), click(), should(), clear() e comandos customizados

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,11 +1,28 @@
 describe('Central de Atendimento ao Cliente TAT', () => {
   // Variáveis de apoio
+  const utilData = {
+    title: 'Central de Atendimento ao Cliente TAT',
+    firstName: 'David',
+    lastName: 'Teixeira de Masin',
+    invalidEmail: 'davidteixeira.info@gmail,com',
+    validEmail: 'davidteixeira.info@gmail.com',
+    phone: '85989647993',
+    invalidPhone: 'oitocinconoveoitonovemeiaquatrosetenovenovetrês',
+    shortMessage: 'Alguma coisa aqui para fins de teste',
+    longMessage: Cypress._.repeat('Testando 123 qwerty abcdefghijklmnopqrstuvwxyz', 10),
+    products: [
+      'Blog', 'Cursos', 'Mentoria', 'YouTube'
+    ]
+  }
+
   const title = 'Central de Atendimento ao Cliente TAT'
   const firstName = 'David'
   const lastName = 'Teixeira de Masin'
-  const email = 'davidteixeira.info@gmail.com'
+  const invalidEmail = 'davidteixeira.info@gmail,com'
+  const validEmail = 'davidteixeira.info@gmail.com'
   const phone = '85989647993'
-  const message = 'Alguma coisa aqui para fins de teste'
+  const invalidPhone = 'oitocinconoveoitonovemeiaquatrosetenovenovetrês'
+  const shortMessage = 'Alguma coisa aqui para fins de teste'
   const longMessage = Cypress._.repeat('Testando 123 qwerty abcdefghijklmnopqrstuvwxyz', 10)
   const products = ['Blog', 'Cursos', 'Mentoria', 'YouTube']
   
@@ -16,7 +33,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
       .should('be.equal', title)
   })
 
-  it.only('preenche os campos obrigatório e envia o formulário', () => {
+  it('preenche os campos obrigatório e envia o formulário', () => {
     cy.get('#firstName').as('campo_nome')
     cy.get('@campo_nome').should('be.visible')
     cy.get('@campo_nome').type(firstName)
@@ -29,14 +46,8 @@ describe('Central de Atendimento ao Cliente TAT', () => {
 
     cy.get('#email').as('campo_email')
     cy.get('@campo_email').should('be.visible')
-    cy.get('@campo_email').type(email)
-    cy.get('@campo_email').should('have.value', email)
-
-    // cy.get('#phone').as('campo_telefone')
-    // cy.get('@campo_telefone').should('be.visible')
-    // cy.get('@campo_telefone')
-    //   .type(phone)
-    //   .should('have.value', phone)
+    cy.get('@campo_email').type(validEmail)
+    cy.get('@campo_email').should('have.value', validEmail)
 
     // cy.get('#product').as('seleção_produtos')
     // cy.get('@seleção_produtos').should('be.visible')
@@ -47,16 +58,12 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     //   .should('be.visible')
     //   .click()
 
-    // cy.get("#phone-checkbox")
-    //   .should('be.visible')
-    //   .click()
-
     cy.get('#open-text-area').as('como-podemos-te-ajudar')
     cy.get('@como-podemos-te-ajudar').should('be.visible')
     cy.get('@como-podemos-te-ajudar').type(longMessage, {delay: 0})
     cy.get('@como-podemos-te-ajudar').should('have.value', longMessage)
 
-    cy.get('.button').as('enviar-form')
+    cy.get('button[type="submit"]').as('enviar-form')
     cy.get('@enviar-form').should('be.visible')
     cy.get('@enviar-form').click()
 
@@ -78,30 +85,64 @@ describe('Central de Atendimento ao Cliente TAT', () => {
 
     cy.get('#email').as('campo_email')
     cy.get('@campo_email').should('be.visible')
-    cy.get('@campo_email').type('email')
-    cy.get('@campo_email').should('have.value', 'email')
+    cy.get('@campo_email').type(invalidEmail)
+    cy.get('@campo_email').should('have.value', invalidEmail)
+    cy.get('@campo_email').should('not.be.equal', validEmail)
 
     cy.get('#open-text-area').as('como-podemos-te-ajudar')
     cy.get('@como-podemos-te-ajudar').should('be.visible')
-    cy.get('@como-podemos-te-ajudar').type(longMessage, {delay: 2})
-    cy.get('@como-podemos-te-ajudar').should('have.value', longMessage)
+    cy.get('@como-podemos-te-ajudar').type(shortMessage, {delay: 2})
+    cy.get('@como-podemos-te-ajudar').should('have.value', shortMessage)
 
-    cy.get('.button').as('enviar-form')
+    cy.get('button[type="submit"]').as('enviar-form')
     cy.get('@enviar-form').should('be.visible')
     cy.get('@enviar-form').click()
 
     cy.get('.error > strong').as('msg-de-erro')
     cy.get('@msg-de-erro').should('be.visible')
     cy.get('@msg-de-erro').should('have.text', 'Valide os campos obrigatórios!')
-
   });
 
-  it('verifica se o campo telefone continua vazio ao digitar valor não-numérico', () => {
+  it('campo telefone continua vazio quando preenchido com um valor não-numérico', () => {
     cy.get('#phone').as('campo_telefone')
     cy.get('@campo_telefone').should('be.visible')
-    cy.get('@campo_telefone').type('phone')
-    cy.get('@campo_telefone').should('not.have.value', 'phone')
+    cy.get('@campo_telefone').type(invalidPhone)
+    cy.get('@campo_telefone').should('not.have.value', invalidPhone)
     cy.get('@campo_telefone').should('have.value', '')
+  });
+
+  it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+    cy.get('#firstName').as('campo_nome')
+    cy.get('@campo_nome').should('be.visible')
+    cy.get('@campo_nome').type(firstName)
+    cy.get('@campo_nome').should('have.value', firstName)
+
+    cy.get('#lastName').as('campo_sobrenome')
+    cy.get('@campo_sobrenome').should('be.visible')
+    cy.get('@campo_sobrenome').type(lastName)
+    cy.get('@campo_sobrenome').should('have.value', lastName)
+
+    cy.get('#email').as('campo_email')
+    cy.get('@campo_email').should('be.visible')
+    cy.get('@campo_email').type(validEmail)
+    cy.get('@campo_email').should('have.value', validEmail)
+
+    cy.get("#phone-checkbox").as('marcador-telefone')
+    cy.get('@marcador-telefone').should('be.visible')
+    cy.get('@marcador-telefone').click()
+
+    cy.get('#open-text-area').as('como-podemos-te-ajudar')
+    cy.get('@como-podemos-te-ajudar').should('be.visible')
+    cy.get('@como-podemos-te-ajudar').type(longMessage, {delay: 0})
+    cy.get('@como-podemos-te-ajudar').should('have.value', longMessage)
+
+    cy.get('button[type="submit"]').as('enviar-form')
+    cy.get('@enviar-form').should('be.visible')
+    cy.get('@enviar-form').click()
+
+    cy.get('.error > strong').as('msg-de-erro')
+    cy.get('@msg-de-erro').should('be.visible')
+    cy.get('@msg-de-erro').should('have.text', 'Valide os campos obrigatórios!')
   });
 
   it('preenche e limpa os campos nome, sobrenome, email e telefone', () => {
@@ -117,8 +158,8 @@ describe('Central de Atendimento ao Cliente TAT', () => {
 
     cy.get('#email').as('campo_email')
     cy.get('@campo_email').should('be.visible')
-    cy.get('@campo_email').type(email)
-    cy.get('@campo_email').should('have.value', email)
+    cy.get('@campo_email').type(validEmail)
+    cy.get('@campo_email').should('have.value', validEmail)
 
     cy.get('#phone').as('campo_telefone')
     cy.get('@campo_telefone').should('be.visible')
@@ -139,7 +180,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
   });
 
   it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
-    cy.get('.button').as('enviar-form')
+    cy.get('button[type="submit"]').as('enviar-form')
     cy.get('@enviar-form').click()
 
     cy.get('.error > strong').as('msg-de-erro')
@@ -148,7 +189,12 @@ describe('Central de Atendimento ao Cliente TAT', () => {
   });
   
   it('envia o formulário com sucesso usando um comando customizado', () => {
-    
+    // cy.fillMandatoryFieldsAndSubmit(utilData)
+    cy.fillMandatoryFieldsAndSubmit()
+
+    cy.get('.success > strong').as('msg-de-sucesso')
+    cy.get('@msg-de-sucesso').should('be.visible')
+    cy.get('@msg-de-sucesso').should('have.text', 'Mensagem enviada com sucesso.')
   });
 
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,34 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', (data = {
+    firstName: 'Xablau',
+    lastName: 'Master',
+    validEmail: 'xablaumaster@teste.com',
+    shortMessage: 'Teste QA'
+}) => {
+    cy.get('#firstName').as('campo_nome')
+    cy.get('@campo_nome').should('be.visible')
+    cy.get('@campo_nome').type(data.firstName)
+    cy.get('@campo_nome').should('have.value', data.firstName)
+
+    cy.get('#lastName').as('campo_sobrenome')
+    cy.get('@campo_sobrenome').should('be.visible')
+    cy.get('@campo_sobrenome').type(data.lastName)
+    cy.get('@campo_sobrenome').should('have.value', data.lastName)
+
+    cy.get('#email').as('campo_email')
+    cy.get('@campo_email').should('be.visible')
+    cy.get('@campo_email').type(data.validEmail)
+    cy.get('@campo_email').should('have.value', data.validEmail)
+
+    cy.get('#open-text-area').as('como-podemos-te-ajudar')
+    cy.get('@como-podemos-te-ajudar').should('be.visible')
+    cy.get('@como-podemos-te-ajudar').type(data.shortMessage, {delay: 0})
+    cy.get('@como-podemos-te-ajudar').should('have.value', data.shortMessage)
+
+    cy.get('button[type="submit"]').as('enviar-form')
+    cy.get('@enviar-form').should('be.visible')
+    cy.get('@enviar-form').click()
+})
